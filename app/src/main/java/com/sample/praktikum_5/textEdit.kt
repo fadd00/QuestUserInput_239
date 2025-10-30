c42ca339502a7feded3e5ed2090bfb6eb5a18cb4
fadd00 <andhikahutama9@gmail.com>
2025-10-30 09:30:51 +0700

    Add ui-graphics dependency and update FormDataDiri composable with custom divider color

--- app/src/main/java/com/sample/praktikum_5/textEdit.kt
+++ app/src/main/java/com/sample/praktikum_5/textEdit.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
@@ -81,7 +82,9 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(
                 bottom = dimensionResource(R.dimen.padding_medium),
                 top = dimensionResource(R.dimen.padding_medium)
-            )
+            ),
+            thickness = dimensionResource( id = R.dimen.divider_tipis),
+            color = Color.darkGray
         )
     }
 }
