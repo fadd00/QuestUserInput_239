c53406acf232c5205550a82aee57fada4527548c
fadd00 <andhikahutama9@gmail.com>
2025-10-30 08:54:36 +0700

    Add gender list to FormDataDiri composable function

--- app/src/main/java/com/sample/praktikum_5/textEdit.kt
+++ app/src/main/java/com/sample/praktikum_5/textEdit.kt
@@ -15,4 +15,6 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
+
+    val gender:list<String> = listOf("Laki-laki","Perempuan")
 }
