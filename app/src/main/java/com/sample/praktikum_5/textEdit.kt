41ae13950eef68c5a86fcbb841cab12f42307723
fadd00 <andhikahutama9@gmail.com>
2025-10-30 08:51:02 +0700

    Add FormDataFiri composable function for text input handling

--- app/src/main/java/com/sample/praktikum_5/textEdit.kt
+++ app/src/main/java/com/sample/praktikum_5/textEdit.kt
@@ -1,3 +1,11 @@
 package com.sample.praktikum_5
 
-c
+import androidx.compose.ui.Modifier
+import java.text.Normalizer
+
+@composable
+fun FormDataFiri(modifier: Modifier
+){
+    //variable - variable untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+}
