3c805762fb47f32c3c4d668876cb7ec3b47eeb80
fadd00 <andhikahutama9@gmail.com>
2025-10-30 08:53:44 +0700

    Rename FormDataFiri to FormDataDiri and add state variables for user input

--- app/src/main/java/com/sample/praktikum_5/textEdit.kt
+++ app/src/main/java/com/sample/praktikum_5/textEdit.kt
@@ -4,8 +4,15 @@ import androidx.compose.ui.Modifier
 import java.text.Normalizer
 
 @composable
-fun FormDataFiri(modifier: Modifier
+fun FormDataDiri(modifier: Modifier
 ){
     //variable - variable untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    //variable -variable untuk menyimpan data yang diperoleh dari komponer
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 }
